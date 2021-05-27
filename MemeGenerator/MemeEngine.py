af247c66840dc3f66bf73e5c939c27ea2b2b5252
BCT <bertrand.chevalier@gmail.com>
2021-05-27 16:56:05 -0400

    Documented code

--- MemeGenerator/MemeEngine.py
+++ MemeGenerator/MemeEngine.py
@@ -1,12 +1,42 @@
+"""
+Create meme.
+
+Resize the image to standard and include
+quote provided by the QuoteEngine
+"""
+
 from PIL import Image, ImageDraw, ImageFont
 import os
 
 
 class MemeEngine():
+    """
+    Class to create meme.
+
+    Given an image and a quote, will resize the image and add
+    the quote
+    """
+
     def __init__(self, output_dir):
+        """
+        Initiate an instance of the class.
+        
+        Arg: output_dir: folder where created meme will be dropped
+        """
         self.output_dir = output_dir
 
     def make_meme(self, img_path, text, author, width=500) -> str:
+        """
+        Create meme.
+
+        Take an image, open it, extract its name to build output path
+        Add text and author, save it and return its path
+        Args:
+        img_path: input image
+        text: body of the quote
+        author: author of the quote
+        width: max width the output image (meme) will have
+        """
         img = Image.open(img_path)
         img_name = img_path.split('/')[-1]
         font = ImageFont.truetype('./_data/Font/LilitaOne-Regular.ttf', 36)
@@ -28,6 +58,12 @@ class MemeEngine():
         return output_path
 
     def create_output_path(self, img_name):
+        """
+        Create output file path.
+
+        Handle the creation of the meme output path.
+        Takes the image and rename it "meme_"image
+        """
         try:
             os.mkdir(self.output_dir)
         except Exception as e:
--- MemeGenerator/__init__.py
+++ MemeGenerator/__init__.py
@@ -1,1 +1,8 @@
+"""
+Creates Meme.
+
+Resize the image to standard and include
+quote provided by the QuoteEngine
+"""
+
 from .MemeEngine import MemeEngine
